7bf89980991c64888e3f8caac110c999431c0c86
DANIEL21NAULA <163338712+DANIEL21NAULA@users.noreply.github.com>
2026-01-08 11:36:36 -0500

    environment configuration variables

--- README.md
+++ README.md
@@ -24,6 +24,21 @@ $ npm i - g @nestjs/cli
 ```bash
 $ docker-compose up -d
 ```
+5. Clonar el archivo __.env.template__ y renombrar la copia a __.env__
+
+6. Llenar las variables de entorno definidas en el __.env__
+
+7. Ejecutar la aplicacion en dev:
+```bash 
+# watch mode
+$ yarn run start:dev
+```
+
+8. Reconstruir la base de datos con la semilla
+```
+http://localhost:3000/api/v2/seed
+```
+
 
 
 ## Compile and run the project
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -3,11 +3,13 @@ import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
   imports: [
+    ConfigModule, //! esta linea es para poder utilizar las variables del env.config.ts
     //* con esta linea de codigo se crea la tabla en la BD
     MongooseModule.forFeature([
       {
